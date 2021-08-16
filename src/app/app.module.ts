0b6af88d3eed149cf6be4fbe27e59edb37e443a3
Nilkanteshwar <nilkant678@gmail.com>
2021-08-16 21:26:49 +0530

    this is new updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+// import{firebaseConfig} from '../environments/environment';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { Camera } from '@ionic-native/camera/ngx';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
-
+// import {AngularFireAuthModule} from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },Camera
+  , FormsModule  , LocalNotifications 
   ],
   bootstrap: [AppComponent],
 })
